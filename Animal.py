3abcddf2ed4eda44c049fd1cb7b9b1e105d77c08
Ayub Tursonzadah <ayubtursonzadah@gmail.com>
2025-02-20 23:24:37 -0500

    plz work lol

--- Animal.py
+++ Animal.py
@@ -4,5 +4,23 @@ class Animal:
         print("hello, I am", self.__name)
 
     def talk(self):
-        print("hi")
+        print("WOOF!")
+#Addtional 5 functions
+    
+    def eat(self, food):
+        print(f"{self.__name} eats {food}.")  
+        
+    def sleep(self, hours):
+        print(f"{self.__name} sleeps for {hours} hours.")
+
+    def play(self):
+        print(f"{self.__name} is playing fetch!")
+
+    def groom(self):
+        print(f"{self.__name} is cleaning himself with water.")
+
+    def make_sound(self):
+        print(f"{self.__name} makes a barking sound.")
+
+              
 
